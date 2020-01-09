b5f1bf3559a0422932d84c878d1dd638920d251f
Stugeh <tuukkaveteli@gmail.com>
2020-01-10 00:31:00 +0200

    unit conversions etc.

--- src/main/java/com/vogella/maven/NewWeatherApp/WeatherMain.java
+++ src/main/java/com/vogella/maven/NewWeatherApp/WeatherMain.java
@@ -3,6 +3,14 @@ package com.vogella.maven.NewWeatherApp;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 public class WeatherMain {
 	
 	public static void main(String[] args) throws Exception{
@@ -10,7 +18,24 @@ public class WeatherMain {
 		HttpResponse<kong.unirest.JsonNode> response = Unirest.get("https://api.darksky.net/forecast/847c94249ad8a47be419557c13ede9a3/65.5267,25.4233").asJson(); 		
 		WeatherData weather = new WeatherData(response);
 		System.out.println(weather);
-	
-	}
-}
+	    JFrame f = new JFrame("A JFrame");
+	    f.setSize(250, 250);
+	    f.setLocation(300,200);
+	    final JTextArea textArea = new JTextArea(10, 40);
+	    f.getContentPane().add(BorderLayout.CENTER, textArea);
+	    final JButton button = new JButton("Click Me");
+	    f.getContentPane().add(BorderLayout.SOUTH, button);
+	    button.addActionListener(new ActionListener() {
 
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	            textArea.append("Button was clicked\n");
+
+	        }
+	    });
+
+	    f.setVisible(true);
+
+	  }
+
+	}
