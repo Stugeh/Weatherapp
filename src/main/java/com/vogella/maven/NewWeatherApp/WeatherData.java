9531da94eef3b51b56c516c8ed4ecab5c22e5d2b
Stugeh <tuukkaveteli@gmail.com>
2020-01-09 23:38:48 +0200

    unit conversions etc.

--- src/main/java/com/vogella/maven/NewWeatherApp/WeatherData.java
+++ src/main/java/com/vogella/maven/NewWeatherApp/WeatherData.java
@@ -4,187 +4,251 @@ import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.json.JSONObject;
 
+import java.time.LocalTime;;
+
 public class WeatherData {
-	
+
 	private String dlySummary, hrlySummary, crntSummary;
-	private String temperature, time, precipIntensity, precipProbability, apparentTemperature, humidity, windSpeed, windBearing, cloudCover, visibility, ozone;
-	
+	private String time, precipIntensity, precipProbability, humidity, windBearing, cloudCover, visibility, ozone;
+	private double apparentTemperature, temperature, windSpeed;
+	private LocalTime clock;
+
 	public WeatherData(HttpResponse<JsonNode> response) {
-		JSONObject data = response.getBody().getObject();		
+		JSONObject data = response.getBody().getObject();
 		JSONObject current = (JSONObject) data.get("currently");
 		JSONObject hourly = (JSONObject) data.get("hourly");
 		JSONObject daily = (JSONObject) data.get("daily");
 		
-		
-		System.out.println("\n\n");
-		System.out.println(current.keySet());
-		System.out.println("\n\n");
-		
-		this.apparentTemperature = current.getString("apparentTemperature");
+		this.setClock(LocalTime.now());
+		this.temperature = (current.getDouble("temperature") - 32) * 5 / 9;
+		this.apparentTemperature = (current.getDouble("apparentTemperature") - 32) * 5 / 9;
 		this.cloudCover = current.getString("cloudCover");
 		this.humidity = current.getString("humidity");
 		this.ozone = current.getString("ozone");
 		this.precipIntensity = current.getString("precipIntensity");
 		this.precipProbability = current.getString("precipProbability");
-		this.temperature = current.getString("temperature");
 		this.time = current.getString("time");
 		this.visibility = current.getString("visibility");
+		this.windSpeed = current.getDouble("windSpeed") * 0.44704;
 		this.windBearing = current.getString("windBearing");
-		this.windSpeed = current.getString("windSpeed");
 		this.dlySummary = daily.getString("summary");
 		this.hrlySummary = hourly.getString("summary");
 		this.crntSummary = current.getString("summary");
-    }
+	}
 
 	@Override
 	public String toString() {
-		return "WeatherData [dlySummary=" + dlySummary + ", hrlySummary=" + hrlySummary + ", crntSummary=" + crntSummary
-				+ ", temperature=" + temperature + ", time=" + time + ", precipIntensity=" + precipIntensity
-				+ ", precipProbability=" + precipProbability + ", apparentTemperature=" + apparentTemperature
-				+ ", humidity=" + humidity + ", windSpeed=" + windSpeed + ", windBearing=" + windBearing
-				+ ", cloudCover=" + cloudCover + ", visibility=" + visibility + ", ozone=" + ozone + "]";
+		return clock + "\n\ndaily Summary=" + dlySummary + " \nhourly Summary=" + hrlySummary + " \ncurrent Summary="
+				+ crntSummary + " \n\ntemperature=" + temperature + " \napparentTemperature=" + apparentTemperature
+				+ " \n\nprecipIntensity=" + precipIntensity + " \nprecipProbability=" + precipProbability
+				+ " \nhumidity=" + humidity + " \n\nwindSpeed=" + windSpeed + " \nwindBearing=" + windBearing
+				+ " \n\ncloudCover=" + cloudCover + " \nvisibility=" + visibility + " \nozone=" + ozone;
 	}
 
-	
-	/*Getters and setters*/
-	
-	
-	public String getTime() {
-		return time;
-	}
+	/* Getters and setters */
 
-
-	public void setTime(String time) {
-		this.time = time;
-	}
-
-
-	public String getPrecipIntensity() {
-		return precipIntensity;
-	}
-
-
-	public void setPrecipIntensity(String precipIntensity) {
-		this.precipIntensity = precipIntensity;
-	}
-
-
-	public String getPrecipProbability() {
-		return precipProbability;
-	}
-
-
-	public void setPrecipProbability(String precipProbability) {
-		this.precipProbability = precipProbability;
-	}
-
-
-	public String getApparentTemperature() {
-		return apparentTemperature;
-	}
-
-
-	public void setApparentTemperature(String apparentTemperature) {
-		this.apparentTemperature = apparentTemperature;
-	}
-
-
-	public String getHumidity() {
-		return humidity;
-	}
-
-
-	public void setHumidity(String humidity) {
-		this.humidity = humidity;
-	}
-
-
-	public String getWindBearing() {
-		return windBearing;
-	}
-
-
-	public void setWindBearing(String windBearing) {
-		this.windBearing = windBearing;
-	}
-
-
-	public String getCloudCover() {
-		return cloudCover;
-	}
-
-
-	public void setCloudCover(String cloudCover) {
-		this.cloudCover = cloudCover;
-	}
-
-
-	public String getVisibility() {
-		return visibility;
-	}
-
-
-	public void setVisibility(String visibility) {
-		this.visibility = visibility;
-	}
-
-
-	public String getOzone() {
-		return ozone;
-	}
-
-
-	public void setOzone(String ozone) {
-		this.ozone = ozone;
-	}
-
-
+	/**
+	 * @return the dlySummary
+	 */
 	public String getDlySummary() {
 		return dlySummary;
 	}
 
-
+	/**
+	 * @param dlySummary the dlySummary to set
+	 */
 	public void setDlySummary(String dlySummary) {
 		this.dlySummary = dlySummary;
 	}
 
-
-	public String getTemperature() {
-		return temperature;
-	}
-
-
-	public void setTemperature(String temperature) {
-		this.temperature = temperature;
-	}
-
-
-	public String getWindSpeed() {
-		return windSpeed;
-	}
-
-
-	public void setWindSpeed(String windSpeed) {
-		this.windSpeed = windSpeed;
-	}
-
-
+	/**
+	 * @return the hrlySummary
+	 */
 	public String getHrlySummary() {
 		return hrlySummary;
 	}
 
-
+	/**
+	 * @param hrlySummary the hrlySummary to set
+	 */
 	public void setHrlySummary(String hrlySummary) {
 		this.hrlySummary = hrlySummary;
 	}
 
-
+	/**
+	 * @return the crntSummary
+	 */
 	public String getCrntSummary() {
 		return crntSummary;
 	}
 
-
+	/**
+	 * @param crntSummary the crntSummary to set
+	 */
 	public void setCrntSummary(String crntSummary) {
 		this.crntSummary = crntSummary;
 	}
+
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return time;
+	}
+
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	/**
+	 * @return the precipIntensity
+	 */
+	public String getPrecipIntensity() {
+		return precipIntensity;
+	}
+
+	/**
+	 * @param precipIntensity the precipIntensity to set
+	 */
+	public void setPrecipIntensity(String precipIntensity) {
+		this.precipIntensity = precipIntensity;
+	}
+
+	/**
+	 * @return the precipProbability
+	 */
+	public String getPrecipProbability() {
+		return precipProbability;
+	}
+
+	/**
+	 * @param precipProbability the precipProbability to set
+	 */
+	public void setPrecipProbability(String precipProbability) {
+		this.precipProbability = precipProbability;
+	}
+
+	/**
+	 * @return the humidity
+	 */
+	public String getHumidity() {
+		return humidity;
+	}
+
+	/**
+	 * @param humidity the humidity to set
+	 */
+	public void setHumidity(String humidity) {
+		this.humidity = humidity;
+	}
+
+	/**
+	 * @return the windSpeed
+	 */
+	public double getWindSpeed() {
+		return windSpeed;
+	}
+
+	/**
+	 * @param windSpeed the windSpeed to set
+	 */
+	public void setWindSpeed(double windSpeed) {
+		this.windSpeed = windSpeed;
+	}
+
+	/**
+	 * @return the windBearing
+	 */
+	public String getWindBearing() {
+		return windBearing;
+	}
+
+	/**
+	 * @param windBearing the windBearing to set
+	 */
+	public void setWindBearing(String windBearing) {
+		this.windBearing = windBearing;
+	}
+
+	/**
+	 * @return the cloudCover
+	 */
+	public String getCloudCover() {
+		return cloudCover;
+	}
+
+	/**
+	 * @param cloudCover the cloudCover to set
+	 */
+	public void setCloudCover(String cloudCover) {
+		this.cloudCover = cloudCover;
+	}
+
+	/**
+	 * @return the visibility
+	 */
+	public String getVisibility() {
+		return visibility;
+	}
+
+	/**
+	 * @param visibility the visibility to set
+	 */
+	public void setVisibility(String visibility) {
+		this.visibility = visibility;
+	}
+
+	/**
+	 * @return the ozone
+	 */
+	public String getOzone() {
+		return ozone;
+	}
+
+	/**
+	 * @param ozone the ozone to set
+	 */
+	public void setOzone(String ozone) {
+		this.ozone = ozone;
+	}
+
+	/**
+	 * @return the apparentTemperature
+	 */
+	public double getApparentTemperature() {
+		return apparentTemperature;
+	}
+
+	/**
+	 * @param apparentTemperature the apparentTemperature to set
+	 */
+	public void setApparentTemperature(double apparentTemperature) {
+		this.apparentTemperature = apparentTemperature;
+	}
+
+	/**
+	 * @return the temperature
+	 */
+	public double getTemperature() {
+		return temperature;
+	}
+
+	/**
+	 * @param temperature the temperature to set
+	 */
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+
+	public LocalTime getClock() {
+		return clock;
+	}
+
+	public void setClock(LocalTime clock) {
+		this.clock = clock;
+	}
+
 }
--- src/main/java/com/vogella/maven/NewWeatherApp/WeatherMain.java
+++ src/main/java/com/vogella/maven/NewWeatherApp/WeatherMain.java
@@ -10,24 +10,6 @@ public class WeatherMain {
 		HttpResponse<kong.unirest.JsonNode> response = Unirest.get("https://api.darksky.net/forecast/847c94249ad8a47be419557c13ede9a3/65.5267,25.4233").asJson(); 		
 		WeatherData weather = new WeatherData(response);
 		System.out.println(weather);
-
-		
-		
-				
-				
-		//WeatherData currently = new WeatherData(response, "currently");
-				 
-				 
-		//response = Unirest.toString(response);
-		//System.out.printf(temperature, "C\n", summary );
-		
-		
-		System.out.println(response.getBody().getObject().keySet());
-		//System.out.println(response.getBody().getObject().get("Hourly"["Summary"]
-			//	);
-		
-		
-		
 	
 	}
 }
