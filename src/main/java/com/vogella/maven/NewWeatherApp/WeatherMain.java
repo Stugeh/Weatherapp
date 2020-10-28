c4077c2cab690c46d3cedfcf3898f780713ae0a5
Stugeh <tuukkaveteli@gmail.com>
2020-10-28 15:40:57 +0200

    Update WeatherMain.java

--- src/main/java/com/vogella/maven/NewWeatherApp/WeatherMain.java
+++ src/main/java/com/vogella/maven/NewWeatherApp/WeatherMain.java
@@ -12,7 +12,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class WeatherMain {
 	
 	public static void main(String[] args) throws Exception{
-		String token = "NjYzNzg5MjMwMDczNTc3NTE0.Xh33AA.JWD8f3Td2svoDStIvu7nP5NK-uc";
 		HttpResponse<kong.unirest.JsonNode> response = Unirest.get("https://api.darksky.net/forecast/847c94249ad8a47be419557c13ede9a3/65.5267,25.4233").asJson(); 		
 		JDA jda = new JDABuilder(token).build();
 		jda.addEventListener(new DiscordBot());
